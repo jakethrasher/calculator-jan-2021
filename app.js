ba66f4f22164ae86cd28ad0891145ac62ba633ea
Jake Thrasher <mjakethrasher@gmail.com>
2021-01-07 14:35:55 -0800

    add clear button

--- app.js
+++ app.js
@@ -9,50 +9,57 @@ const resultsDisplay = document.getElementById('results');
 
 // event handlers for each button redundantly writen out for practice 
 
-addButton.addEventListener('click', () => {
-    let result = Number(inputOne.value) + Number(inputTwo.value);
-    resultsDisplay.innerHTML = result;
+// addButton.addEventListener('click', () => {
+//     let result = Number(inputOne.value) + Number(inputTwo.value);
+//     resultsDisplay.innerHTML = result;
 
-});
-subtractButton.addEventListener('click', () => {
-    let result = Number(inputOne.value) - Number(inputTwo.value);
-    resultsDisplay.innerHTML = result;
+// });
+// subtractButton.addEventListener('click', () => {
+//     let result = Number(inputOne.value) - Number(inputTwo.value);
+//     resultsDisplay.innerHTML = result;
 
-});
-multiplyButton.addEventListener('click', () => {
-    let result = Number(inputOne.value) * Number(inputTwo.value);
-    resultsDisplay.innerHTML = result;
+// });
+// multiplyButton.addEventListener('click', () => {
+//     let result = Number(inputOne.value) * Number(inputTwo.value);
+//     resultsDisplay.innerHTML = result;
 
-});
-divideButton.addEventListener('click', () => {
-    let result = Number(inputOne.value) / Number(inputTwo.value);
-    resultsDisplay.innerHTML = result;
+// });
+// divideButton.addEventListener('click', () => {
+//     let result = Number(inputOne.value) / Number(inputTwo.value);
+//     resultsDisplay.innerHTML = result;
 
-});
+// });
 
 
-// looping through the array of buttons 
+//looping through the array of buttons 
 
-// const buttonsArray = document.querySelectorAll('button');
+const buttonsArray = document.querySelectorAll('button');
 
-// for (let button of buttonsArray) {
+for (let button of buttonsArray) {
 
-//     button.addEventListener('click', () => {
-//         let numOne = Number(inputOne.value);
-//         let numTwo = Number(inputTwo.value);
-//         let result;
-//         if (button.id === 'add') {
-//             result = numOne + numTwo;
-//         } else if (button.id === 'subtract') {
-//             result = numOne - numTwo;
-//         } else if (button.id === 'multiply') {
-//             result = numOne * numTwo;
-//         } else if (button.id === 'divide') {
-//             result = numOne / numTwo;
-//         }
-//         resultsDisplay.textContent = result;
-//     });
-// }
+    button.addEventListener('click', () => {
+        
+        let numOne = Number(inputOne.value);
+        let numTwo = Number(inputTwo.value);
+        
+        let result;
+        
+        if (button.id === 'add') {
+            result = numOne + numTwo;
+        } else if (button.id === 'subtract') {
+            result = numOne - numTwo;
+        } else if (button.id === 'multiply') {
+            result = numOne * numTwo;
+        } else if (button.id === 'divide') {
+            result = numOne / numTwo;
+        } else if (button.id === 'clear'){
+            resultsDisplay.textContent = '';
+            inputTwo.value = '';
+            inputTwo.value = '';
+        }
+        resultsDisplay.textContent = result;
+    });
+}
 
 
 
