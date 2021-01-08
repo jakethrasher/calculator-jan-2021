748d20bbac70cc2724f0cfa4f274b55eed2562e1
Jake Thrasher <mjakethrasher@gmail.com>
2021-01-08 14:04:29 -0800

    delete unnecessary semicolons

--- utils.js
+++ utils.js
@@ -1,18 +1,18 @@
 export function addFunc(num1, num2) {
     return num1 + num2;
-};
+}
 
-export function subtractFunc(num1, num2){
+export function subtractFunc(num1, num2) {
     return num1 - num2;
-};
+}
 
 
 export function multiplyFunc(num1, num2) {
     return num1 * num2;
-};
+}
 
 export function divideFunc(num1, num2) {
     return num1 / num2;
-};
+}
 
 
